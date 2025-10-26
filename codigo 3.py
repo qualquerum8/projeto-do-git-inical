d4a5a7be21dc7f1243922181c83344e8409f6787
qualquerum8 <pcdoarthur92@gmail.com>
2025-10-26 20:54:02 -0300

    commit inical

---
+++ codigo 3.py
@@ -0,0 +1,8 @@
+#essa vai ser uma calculadora so com soma, kkkkkkk
+numero_1 = int(input("digite um numero ai meu amigo "))
+numero_2 = int(input("agora digite mais outro ai meu chapa"))
+#aqui vai ficar os numero que a pessoas vai botar 
+#abaixa vai ser o resultado
+resultado = numero_1 + numero_2
+#mais abaixo fica o print(com o resltado)
+print(f"sua soma é {resultado}")
